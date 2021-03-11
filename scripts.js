4d617755446927f2187d33dde43bdc98e69bc401
Elvis Gannem <egannem@gmail.com>
2021-03-11 11:22:12 -0400

    first-commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+function findLyrics (artist, song) {
+    return fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`)
+}
+
+const form = document.querySelector('#form')
+
+
+
+
+
+form.addEventListener('submit', (element) => {
+    element.preventDefault()
+    getLyrics()
+
+    const lyrics = document.querySelector('#lyrics')
+    lyrics.innerHTML = '<img src="/loading.svg" class="animate-spin w-8 m-auto mt-4">'
+})
+
+async function getLyrics (){
+
+    const artist = document.querySelector('#artist-form')
+    const song = document.querySelector('#song-form')
+    const lyrics = document.querySelector('#lyrics')
+
+    try{
+        const lyricsResponse = await findLyrics(artist.value, song.value)
+        const data = await lyricsResponse.json()
+
+        lyrics.innerHTML = `<p class="mt-4 text-sm text-gray-700">${data.lyrics}</p>`
+    }catch {}
+
+    
+}
+
